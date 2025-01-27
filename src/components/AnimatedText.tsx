6099b7893cbc7d8cb0361a9e5eb9a80cc1abb00c
B G Vinayak <47215953+bhatvinayak@users.noreply.github.com>
2025-01-27 21:08:21 +0530

    Added more animations support

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -8,11 +8,21 @@ type AnimatedTextProps = {
   animationType?: AnimationType; // Type of animation
   duration?: number; // Duration of the animation
   startingDelay?: number; // Delay before starting the animation
+  slideDistance?: number; // Distance for slide animations
+  zoomScale?: number; // Scale for zoom animations
   iterations?: number; // Number of times to repeat the animation
   buffer?: number; // Cooldown between iterations
 };
 
-type AnimationType = "fade-in" | "fade-out" | "none";
+type AnimationType =
+  | "fade-in"
+  | "fade-out"
+  | "slide-up"
+  | "slide-down"
+  | "slide-left"
+  | "slide-right"
+  | "zoom"
+  | "none";
 
 const AnimatedText: React.FC<AnimatedTextProps> = ({
   text,
@@ -21,10 +31,17 @@ const AnimatedText: React.FC<AnimatedTextProps> = ({
   animationType = "none",
   duration = 1000,
   startingDelay = 0,
-  iterations = 1,
+  slideDistance = 0,
+  zoomScale = 1,
+  iterations = Infinity,
   buffer = 0,
 }) => {
-  const animationValue = useRef(new Animated.Value(0)).current;
+  const animationValue = useRef(
+    new Animated.Value(animationType === "fade-out" ? 1 : 0)
+  ).current; // For fade animations
+  const translateXValue = useRef(new Animated.Value(0)).current; // For horizontal slide animations
+  const translateYValue = useRef(new Animated.Value(0)).current; // For vertical slide animations
+  const scaleValue = useRef(new Animated.Value(1)).current; // For zoom animations
 
   useEffect(() => {
     const animation = getAnimation(animationType, duration);
@@ -40,8 +57,7 @@ const AnimatedText: React.FC<AnimatedTextProps> = ({
           { iterations }
         ),
       ]).start();
-    } else if (animationType === "none") {
-      // Set opacity to 1 if no animation is applied
+    } else if (animationType === "none" || animationType === "fade-out") {
       animationValue.setValue(1);
     }
   }, [animationType, duration, startingDelay, iterations]);
@@ -60,20 +76,68 @@ const AnimatedText: React.FC<AnimatedTextProps> = ({
           duration,
           useNativeDriver: true,
         });
+      case "slide-up":
+        return Animated.timing(translateYValue, {
+          toValue: -slideDistance, // Adjust this value as needed
+          duration,
+          useNativeDriver: true,
+        });
+      case "slide-down":
+        return Animated.timing(translateYValue, {
+          toValue: slideDistance, // Adjust this value as needed
+          duration,
+          useNativeDriver: true,
+        });
+      case "slide-left":
+        return Animated.timing(translateXValue, {
+          toValue: -slideDistance, // Adjust this value as needed
+          duration,
+          useNativeDriver: true,
+        });
+      case "slide-right":
+        return Animated.timing(translateXValue, {
+          toValue: slideDistance, // Adjust this value as needed
+          duration,
+          useNativeDriver: true,
+        });
+      case "zoom":
+        return Animated.timing(scaleValue, {
+          toValue: zoomScale,
+          duration,
+          useNativeDriver: true,
+        });
+
       default:
         return undefined;
     }
   };
 
+  const animationStyles = {
+    transform: [
+      {
+        translateY:
+          animationType.includes("slide-up") ||
+          animationType.includes("slide-down")
+            ? translateYValue
+            : 0,
+      },
+      {
+        translateX:
+          animationType.includes("slide-right") ||
+          animationType.includes("slide-left")
+            ? translateXValue
+            : 0,
+      },
+      { scale: animationType === "zoom" ? scaleValue : 1 },
+    ],
+  };
+
+  const opacityStyles = {
+    opacity: animationType.includes("fade-") ? animationValue : 1,
+  };
+
   return (
-    <Animated.View
-      style={[
-        {
-          opacity: animationValue,
-        },
-        containerStyle,
-      ]}
-    >
+    <Animated.View style={[animationStyles, opacityStyles, containerStyle]}>
       <Text style={textStyle}>{text}</Text>
     </Animated.View>
   );
